ef394802124700cddbeca7612f255bb84e817311
Donphili <donphilip65@yahoo.com>
2023-07-20 13:38:12 -0700

    fixed

---
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints a doubly linkedlist of integers
+ *
+ * @h: A pointer to the first component of a list
+ *
+ * Return: The number of the components printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
